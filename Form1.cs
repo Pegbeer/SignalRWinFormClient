33a9f5fda0f1cde880f734123c7e292f9eca1297
Josue <mejia.c@abposus.com>
2021-09-22 15:22:07 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WinFormHub
+{
+    public partial class Form1 : Form
+    {
+        public HubConnection Connection { get; set; }
+        private const string UrlLocal = "https://localhost:44323";
+        public Form1()
+        {
+            InitializeComponent();
+            Connection = new HubConnectionBuilder().WithUrl($"{UrlLocal}/mainhub", options => 
+            {
+                options.Headers.Add("API-KEY", "A3Y3B5P0O0S6KEYDessertpos.com");
+            }).Build();
+            if (Connection.State != HubConnectionState.Connected)
+            {
+                StartHub();
+            }
+        }
+
+        private async void StartHub()
+        {
+            try
+            {
+                await Connection.StartAsync();
+                if (Connection.State == HubConnectionState.Connected)
+                {
+                    Debug.WriteLine("::::::::::::::::::: CONNECTED :::::::::::::::::::::::::::");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"C:\Users\Carlos\Documents\JSONFiles\testOrderKitchen.json";
+                JObject o1 = JObject.Parse(File.ReadAllText(path));
+                string json = o1.ToString(Formatting.None);
+                await Connection.InvokeAsync("SendOrderToKitchen",json,"0000000003");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
